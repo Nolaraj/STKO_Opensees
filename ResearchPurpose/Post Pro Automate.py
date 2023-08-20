5eeec0f39a5b91b1f7a78f767401af74ff96067f
Nolaraj Poudel <pnolaraj@gmail.com>
2023-08-20 14:50:50 +0545

    Update Post Pro Automate.py

--- ResearchPurpose/Post Pro Automate.py
+++ ResearchPurpose/Post Pro Automate.py
@@ -1,15 +1,12 @@
-
 import os
-from time import sleep
 import importlib
 import utils.ThreadUtils
 
 importlib.reload(utils.ThreadUtils)
 import utils.ThreadUtils as tu
-from PySide2.QtCore import Qt, QObject, Signal, Slot, QThread
-from PySide2.QtWidgets import QMessageBox, QApplication
+from PySide2.QtCore import Qt, QObject, Signal, Slot, QThread, QMutex
+from PySide2.QtWidgets import QApplication
 from PyMpc import *
-from PyMpc import MpcOdbVirtualResult as vr
 
 # clear terminal
 App.clearTerminal()
@@ -17,30 +14,28 @@ App.clearTerminal()
 # Exception
 the_exception = None
 
+# Mutex for synchronization
+mutex = QMutex()
+
+# Custom signal for worker finished
+class WorkerFinishedSignal(QObject):
+    finished = Signal()
 
 # my worker class
 class Worker(QObject):
-    # signals
     sendPercentage = Signal(int)
-    finished = Signal()
 
-    print("Inside Worker")
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.worker_finished_signal = WorkerFinishedSignal()
 
-    # lengthy function
-    @Slot()
-    def run(self):
-        print(FileData)
-
-        # enclose your function into a try-except-finally block
-        # so that, in case of exceptions, the finished signal
-        # will always be emitted.
+    @Slot(str, str)  # Modified the slot to accept an additional argument
+    def run(self, CustomArgument):
         try:
             print("I am here")
+            print(CustomArgument)  # Use the custom argument here
 
-
-
-
-
+            # Your lengthy processing code
 
         except Exception as ex:
             # Store the exception outside in the global variable
@@ -48,12 +43,17 @@ class Worker(QObject):
             the_exception = ex
         finally:
             # Done
-            self.finished.emit()
+            self.worker_finished_signal.finished.emit()
 
+# Create the worker thread
+class WorkerThread(QThread):
+    def __init__(self, worker, custom_argument, parent=None):  # Add custom_argument parameter
+        super().__init__(parent)
+        self.worker = worker
+        self.custom_argument = custom_argument
 
-
-
-
+    def run(self):
+        self.worker.run(self.custom_argument)  # Pass both arguments here
 
 #__________________________________________________________________
 
@@ -66,50 +66,42 @@ doc.commitChanges()
 doc.dirty = True
 doc.select(None)
 
-
-
 FileName = 'Main_Path.txt'
 Drift_sp_file = open(FileName, 'r')
 
 lines = Drift_sp_file.readlines()
 print(len(lines))
-for line in lines:
+for index, line in enumerate(lines):
     path = line.strip()
     unicode_path = path.encode('utf-8')
     items = os.listdir(unicode_path)
     from os import *
     for file in items:
-    	fileExt = file.decode()[-5:]
-    	if fileExt == '.mpco':
-    		RecordPath = os.path.join(unicode_path, file)
-    		App.runCommand("OpenDatabase", RecordPath)
-    		break
-    		print(RecordPath)
-    		
+        fileExt = file.decode()[-5:]
+        if fileExt == '.mpco':
+            RecordPath = os.path.join(unicode_path, file)
+            App.runCommand("OpenDatabase", RecordPath)
+            break
 
-    		
-    
+    # Create the worker instance and move it to the worker thread
+    custom_argument = [unicode_path, "Hello"]
+    worker_instance = Worker()
+    thread = WorkerThread(worker_instance, custom_argument)  # Pass the custom argument here
+    worker_instance.moveToThread(thread)
 
+    # Connect the custom finished signal to printing message
+    worker_instance.worker_finished_signal.finished.connect(lambda: print("Done with the work provided"))
 
+    # Start the thread
+    thread.start()
+    thread.wait()
 
-
-    
-    tu.runOnWorkerThread(Worker())
-    
-    print("Done with the work provided")
+    print("Waiting for worker to finish...")
+    mutex.lock()
+    mutex.unlock()
 
     break
 
-
-
-
-
-
-
-
-
-
-# If we catched an exception in the working
-# thread, re-raise it here
+# If we caught an exception in the working thread, re-raise it here
 if the_exception is not None:
     raise the_exception
